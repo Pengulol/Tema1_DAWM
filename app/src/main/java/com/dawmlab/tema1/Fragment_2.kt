c79b8af3ec7bccd6f0ee6e3a02dff2981fab0144
Pengulol <95681649+Pengulol@users.noreply.github.com>
2023-04-15 18:21:56 +0300

    onClick without navigation component

--- app/src/main/java/com/dawmlab/tema1/Fragment_2.kt
+++ app/src/main/java/com/dawmlab/tema1/Fragment_2.kt
@@ -1,4 +1,30 @@
 package com.dawmlab.tema1
 
-class Fragment_2 {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class Fragment_2: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_2, container, false)
+        val animalName = arguments?.getString("animalName")
+        val continentName = arguments?.getString("continentName")
+        val name = view.findViewById<TextView>(R.id.name)
+        val continent = view.findViewById<TextView>(R.id.continent)
+        name.text = animalName
+        continent.text = continentName
+        when (continentName) {
+            "Europe" -> view.setBackgroundResource(R.color.Europa)
+            "Africa" -> view.setBackgroundResource(R.color.Africa)
+            "America" -> view.setBackgroundResource(R.color.Americi)
+            "Asia" -> view.setBackgroundResource(R.color.Asia)
+            "Australia" -> view.setBackgroundResource(R.color.Australia)
+        }
+        return view
+    }
 }
